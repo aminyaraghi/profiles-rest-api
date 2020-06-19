82c0144a1fc816108f97651ac98d0a79af6ebdd8
amin <amin.yaraghi@gmail.com>
2020-06-20 00:40:23 +0430

    Added loggin api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -51,6 +53,7 @@ class HelloApiView(APIView):
         """Delete an object"""
         return Response({'method':'DELETE'})
 
+
 class HelloViewSet(viewsets.ViewSet):
     """Test api ViewSet"""
     serializers_class = serializers.HelloSerializer
@@ -95,6 +98,7 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle destroy an object by its ID"""
         return Response({'http_method':'DELETE'})
 
+
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
@@ -103,3 +107,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tocken"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
